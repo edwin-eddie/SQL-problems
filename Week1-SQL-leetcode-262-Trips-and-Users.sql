a0e105dae70ae849f7b3795e9cd43615061cad52
edwin-eddie <edwin10dsouza@gmail.com>
2025-06-30 16:50:33 +0530

    Update Week1-SQL-leetcode-262-Trips-and-Users.sql

--- Week1-SQL-leetcode-262-Trips-and-Users.sql
+++ Week1-SQL-leetcode-262-Trips-and-Users.sql
@@ -26,3 +26,24 @@ WHERE client_id IN (SELECT users_id FROM valid_users)
   AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY request_at
 ORDER BY request_at;
+
+
+# optimzed further with No Joins , No CTE
+
+SELECT 
+    request_at AS Day,
+    ROUND(
+        SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) * 1.0 
+        / COUNT(*), 
+        2
+    ) AS "Cancellation Rate"
+FROM Trips
+WHERE client_id IN (
+        SELECT users_id FROM Users WHERE banned = 'No'
+    )
+  AND driver_id IN (
+        SELECT users_id FROM Users WHERE banned = 'No'
+    )
+  AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at
+ORDER BY request_at;
