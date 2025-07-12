f5d43c6bc2bb4a9946d0dd9338cc0d531ceb2842
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-12 22:55:59 +0530

    Create week2-sql-practice_4.sql

---
+++ Sql-Practice-Hospital/week2-sql-practice_4.sql
@@ -0,0 +1,6 @@
+# Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+# Primary diagnosis is stored in the admissions table.
+
+select patients.patient_id, first_name, last_name from patients
+inner join admissions on patients.patient_id = admissions.patient_id
+where diagnosis = 'Dementia'
