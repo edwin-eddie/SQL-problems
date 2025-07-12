26351e36f9863b463aa4fb67714baf36b1f7c668
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-12 23:34:59 +0530

    Create week2-sql-practice_9.sql

---
+++ Sql-Practice-Hospital/week2-sql-practice_9.sql
@@ -0,0 +1,6 @@
+# Show the city and the total number of patients in the city.
+# Order from most to least patients and then by city name ascending.
+
+select city, count(*) AS num_patients from patients
+group by city
+order  by num_patients desc, city
