ebcd37768ebed4e7b55bd01705a6cc2667469e96
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-12 22:36:15 +0530

    Update week2-sql-practice.sql

--- Sql-Practice-Hospital/week2-sql-practice.sql
+++ Sql-Practice-Hospital/week2-sql-practice.sql
@@ -3,4 +3,7 @@
 #Login into https://www.sql-practice.com/
   
 #1) Show unique birth years from patients and order them by ascending.
-SELECT distinct(year(birth_date)) FROM patients order by birth_date;
+SELECT distinct 
+year(birth_date) AS birth_year 
+FROM patients 
+order by year(birth_date) ;
