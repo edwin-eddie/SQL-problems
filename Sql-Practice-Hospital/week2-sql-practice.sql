79ef4b297c2f546a5ff611ed10cdd1a7abc44ca3
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-12 22:31:41 +0530

    Update week2-sql-practice.sql

--- Sql-Practice-Hospital/week2-sql-practice.sql
+++ Sql-Practice-Hospital/week2-sql-practice.sql
@@ -1,1 +1,6 @@
 # SQL Practice for Hospital Dataset: 50 Qyestions
+
+#Login into https://www.sql-practice.com/
+  
+#1) Show unique birth years from patients and order them by ascending.
+SELECT distinct(year(birth_date)) FROM patients order by birth_date;
