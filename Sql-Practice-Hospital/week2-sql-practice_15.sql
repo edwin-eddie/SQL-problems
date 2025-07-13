286b7cb3c8e25e9f0bea378a9af414024a7e4c7b
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-13 22:18:36 +0530

    Create week2-sql-practice_15.sql

---
+++ Sql-Practice-Hospital/week2-sql-practice_15.sql
@@ -0,0 +1,5 @@
+# Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
+
+select (max(weight) - min(weight)) as weight_delta
+from patients 
+where last_name = 'Maroni'
