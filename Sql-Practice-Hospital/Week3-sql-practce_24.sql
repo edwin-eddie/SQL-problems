1ce109c0d0e10f45eabe8584ef0289fccfa6d6d7
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-20 12:13:49 +0530

    Create Week3-sql-practce_24.sql

---
+++ Sql-Practice-Hospital/Week3-sql-practce_24.sql
@@ -0,0 +1,16 @@
+
+-- Display patient's full name,
+-- height in the units feet rounded to 1 decimal,
+-- weight in the unit pounds rounded to 0 decimals,
+-- birth_date,
+-- gender non abbreviated.
+
+-- Convert CM to feet by dividing by 30.48.
+-- Convert KG to pounds by multiplying by 2.205.
+
+select concat(first_name,' ', last_name) AS patient_name,
+		round(height/30.48,1) AS 'height "Feet"',
+        round(weight*2.205,0), 
+        birth_date,
+        case when gender= 'M' then 'MALE' else 'FEMALE' end
+from patients
