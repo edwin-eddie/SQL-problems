a527e0611e4d58b93b165d13efd1e38acc4f78a8
edwin-eddie <edwin10dsouza@gmail.com>
2025-07-26 19:34:43 +0530

    Update 176_Second_Highest_Salary.sql

--- Medium-SQL-Leetcode/176_Second_Highest_Salary.sql
+++ Medium-SQL-Leetcode/176_Second_Highest_Salary.sql
@@ -1,3 +1,15 @@
 # 176. Second Highest Salary
 
-select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from  Employee)
+#1) subquery  Approach
+select max(salary) as SecondHighestSalary from Employee where salary < (select distinct max(salary) from  Employee)
+
+
+#2) using RowNumber
+
+with CTE as(
+select 
+    distinct salary,  
+    row_number() over(order by salary desc) as rn
+from Employee)
+select  max(case when rn = 2 then salary else NULL end) as SecondHighestSalary
+from CTE 
